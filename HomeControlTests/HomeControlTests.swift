d212bb2c03b3295b1a43cd3671f13a1b3c6fc682
Felipe Mattos <hi@felipemattos.io>
2016-07-17 20:07:28 -0300

    Adds tests

---
+++ HomeControlTests/HomeControlTests.swift
@@ -0,0 +1,35 @@
+//
+//  HomeControlTests.swift
+//  HomeControlTests
+//
+//  Created by Felipe Mattos at Venturus4Tech Course.
+//
+
+import XCTest
+@testable import HomeControl
+
+class HomeControlTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
